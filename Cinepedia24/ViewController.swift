c0d2364ceb3b962fa079f112203cae22c2a993af
BorisKly <bklykavka@gmail.com>
2024-02-23 13:31:03 +0200

    Initial Commit

---
+++ Cinepedia24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinepedia24
+//
+//  Created by Borys Klykavka on 23.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
